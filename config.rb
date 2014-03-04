f6a4f47f7b7825f2c7af8e0d877ca43dc498458b
Melissa Keizer <mkeizer@rumblelabs.com>
2014-03-04 17:26:59 +0000

    adding project files

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'assets/css/'
+sass_dir = 'assets/sass/'
+images_dir = 'assets/images'
+javascripts_dir = 'assets/js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
